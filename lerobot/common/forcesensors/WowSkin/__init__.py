dbf56bc59781e8c128b0f80f469cc833f5649e49
milong26 <milong26@outlook.com>
2025-06-18 22:33:12 +0800

    wowskin module之前没加好

---
+++ lerobot/common/forcesensors/WowSkin/__init__.py
@@ -0,0 +1,6 @@
+from .sensor import AnySkinBase, AnySkinDummy
+from .sensor_proc import AnySkinProcess
+from .lerobot_wowforcesensor import WowForceSensor
+from .lerobot_wowconfig import WowForceSensorConfig
+
+__all__ = ["AnySkinBase", "AnySkinDummy", "AnySkinProcess"]
